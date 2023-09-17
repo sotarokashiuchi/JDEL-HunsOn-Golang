b4d4ee508c19a44841c9346a7f5128659d1faf15
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-09-18 00:10:33 +0900

    devide fiels

--- analyze.go
+++ analyze.go
@@ -1,192 +1,166 @@
-package main
-
-import "fmt"
-
-// 入力を受付、入力された整数の個数に合わせて各分析用関数を呼び出す関数。
-func main() {
-	var inputNumber []int
-	var inputNumberTotal int
-
-	// input
-	fmt.Printf("解析する整数の個数を入力してください>>>")
-	fmt.Scan(&inputNumberTotal)
-	for i := 0; i < inputNumberTotal; i++ {
-		var input int
-		fmt.Printf("%vつ目の整数を入力してください>>>", i+1)
-		fmt.Scan(&input)
-		inputNumber = append(inputNumber, input)
-	}
-
-	fmt.Println("")
-	fmt.Println("Analyze....")
-	switch inputNumberTotal {
-	case 1:
-		isOddNumber(inputNumber[0])
-		isEvenNumber(inputNumber[0])
-		isPrimeNumber(inputNumber[0])
-		isPerfectNumber(inputNumber[0])
-	case 2:
-		isAmicableNumbers(inputNumber[0], inputNumber[1])
-
-	}
-}
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package analyze
 
 // 奇数判定関数。与えられた引数が奇数の場合、その趣旨を表示する
 // 奇数とは、「2で割り切れない整数」
 // ex) 1, 3, 5, 7, ...
-func isOddNumber(number int) {
+func IsOddNumber(number int) {
 	return
 }
 
 // 偶数判定関数。与えられた引数が偶数の場合、その趣旨を表示する
 // 偶数とは、「2で割り切れる整数」
 // ex) 2, 4, 6, 8, ...
-func isEvenNumber(number int) {
+func IsEvenNumber(number int) {
 	return
 }
 
 // グロタンディーク素数判定関数。与えられた引数がグロタンディーク素数の場合、その趣旨を表示する
 // グロタンディーク素数とは「57の数のこと」
-func isGrothendieckPrime(number int) {
+func IsGrothendieckPrime(number int) {
 	return
 }
 
 // レピュニット数判定関数。与えられた引数がレピュニット数の場合、その趣旨を表示する
 // レピュニット数とは「全ての桁の数字が 1である自然数のこと」
 // ex) 1, 11, 111, ...
-func isRepunitNumber(number int) {
+func IsRepunitNumber(number int) {
 	return
 }
 
 // 素数判定関数。与えられた引数が素数の場合、その趣旨を表示する
 // 素数とは「1を除く約数が1とその数自身だけである自然数」
 // ex) 2, 3, 5, 7, ...
-func isPrimeNumber(number int) {
+func IsPrimeNumber(number int) {
 	return
 }
 
 // 完全数判定関数。与えられた引数が完全数の場合、その趣旨を表示する
 // 完全数とは「自身を除く約数の和が自身に等しい数の自然数」
 // ex) 6, 28, 496, ...
-func isPerfectNumber(number int) {
+func IsPerfectNumber(number int) {
 	return
 }
 
 // 親和数判定関数。与えられた引数が親和数の場合、その趣旨を表示する
 // 親和数とは「自身を除く約数の和が、他方に等しい自然数の組」
 // ex) 220と284, 1184と1210, 2620と2924,...
-func isAmicableNumbers(x, y int) {
+func IsAmicableNumbers(x, y int) {
 	return
 }
 
 // 平方数。与えられた引数が平方数の場合、その趣旨を表示する
 // 平方数とは「整数の自乗（二乗）で表される数である」
 // ex) 0, 1, 4, 9, 16, 25, ...
-func isSquareNumber(number int) {
+func IsSquareNumber(number int) {
 	return
 }
 
 // メルセンヌ数判定関数。与えられた引数がメルセンス数の場合、その趣旨を表示する
 // メルセンヌ数とは「2^n-1で表される数」
 // ex) 1, 3, 7, 31, ...
-func isMersenneNumber(number int) {
+func IsMersenneNumber(number int) {
 	return
 }
 
 // 婚約数判定関数。与えられた引数が婚約数の場合、その趣旨を表示する
 // 婚約数とは「1と自身を除いた約数の和が互いに他方に等しい自然数の組」
 // ex) 48と75, 140と195, 1050と1925, ...
-func isBetrothedNumber(x, y int) {
+func IsBetrothedNumber(x, y int) {
 	return
 }
 
 // ピタゴラス数判定関数。与えられた引数がピタゴラス数の場合、その趣旨を表示する
 // ピタゴラス数とは「a^2 + b^2 = c^2 を満たす3つの自然数の組 (a, b, c) のこと」
 // ex) 3と４と5, 5と12と13, ...
-func isPythagoreanTriple(x, y, z int) {
+func IsPythagoreanTriple(x, y, z int) {
 	return
 }
 
 // セクシー素数判定関数。与えられた引数がセクシー素数の場合、その趣旨を表示する
 // セクシー素数とは「差が6の素数の組(p, p + 6)」
 // ex) 5と11, 7と13, 11と17, ...
-func isSexyPrimes(x, y int) {
+func IsSexyPrimes(x, y int) {
 	return
 }
 
 // 双子素数判定関数。与えられた引数が双子素数の場合、その趣旨を表示する
 // 双子素数とは「差が2である二つの素数の組」
 // ex) 3と5, 5と7, 11と13, ...
-func isTwinPrime(x, y int) {
+func IsTwinPrime(x, y int) {
 	return
 }
 
 // いとこ素数判定関数。与えられた引数がいとこ素数の場合、その趣旨を表示する
 // いとこ素数とは「差が4である素数の組」
 // ex) 3と7, 7と11, 13と17, ...
-func isCousinPrimes(x, y int) {
+func IsCousinPrimes(x, y int) {
 	return
 }
 
 // タクシー数判定関数。与えられた引数がタクシー数の場合、その趣旨を表示する
 // タクシー数とは「2つの立方数の和として表すことができる数(x^3 + y^3 = N)」
 // ex) 2 (= 1^3 + 1^3), 1792 (= 1^3 + 12^3, = 9^3 + 10^3), ...
-func isTaxicabNumber(number int) {
+func IsTaxicabNumber(number int) {
 	return
 }
 
 // ソフィー・ジェルマン素数判定関数。与えられた引数がソフィー・ジェルマン素数の場合、その趣旨を表示する
 // ソフィー・ジェルマン素数とは「pと2p+1が共に素数である時のpのこと」
 // ex) 2, 3, 5, 11, ...
-func isSophieGermainPrime(number int) {
+func IsSophieGermainPrime(number int) {
 	return
 }
 
 // 安全素数判定関数。与えられた引数が安全素数の場合、その趣旨を表示する
 // 安全素数とは「pと2p+1が素数である時の2p+1のこと」
 // ex) 5, 11, 23, 83, ...
-func isSafePrime(number int) {
+func IsSafePrime(number int) {
 	return
 }
 
 // 最小公倍数判定関数。与えられた引数が最小公倍数の場合、その趣旨を表示する
 // 最小公倍数とは「0ではない複数の整数の公倍数のうち共通する最小の自然数を指す」
 // ex) 12と9の最小公倍数は36, ...
-func getLeastCommonMultiple(x, y int) {
+func GetLeastCommonMultiple(x, y int) {
 	return
 }
 
 // 最大公約数判定関数。与えられた引数が最大公約数の場合、その趣旨を表示する
 // 最大公約数とは「２つ以上の正の整数に共通な約数（公約数）のうち最大のもの」
 // ex) 12と9の最大公約数は3, ...
-func getGreatestCommonDivisor(x, y int) {
+func GetGreatestCommonDivisor(x, y int) {
 	return
 }
 
 // カプレカ数ー判定関数。与えられた引数がカプレカ数ーの場合、その趣旨を表示する
 // カプレカ数ーとは「桁を並べ替えて最大にした数と最小にした数との差を取ったとき、元の値に等しくなる自然数」
 // ex) 1, 9, 45, 55, 99, 297, ...
-func isKaprekarNumber(number int) {
+func IsKaprekarNumber(number int) {
 	return
 }
 
 // フィボナッチ数判定関数。与えられた引数がフィボナッチ数の場合、その趣旨を表示する
 // フィボナッチ数とは「フィボナッチ数列の項に該当する数」
 // ex) 0, 1, 1, 2, 3, 5, 8, 13, ...
-func isFibonacciNumber(number int) {
+func IsFibonacciNumber(number int) {
 	return
 }
 
 // フリードマン数判定関数。与えられた引数がフリードマン数の場合、その趣旨を表示する
 // フリードマン数とは「自然数のうち、その数に使われている数字を全て用いて、(I) 四則演算、(II) 累乗、(III) 複数個の数字を合わせて2桁以上の数にする、という3つの方法のうち少なくとも1つを用いて数式を作ることで元の数に一致させられる数のこと。ただし(III)の方法だけでフリードマン数を作ることはできないものとする。」
 // ex) 25 = 5^2 , 121 = 11^2 , 125 = 5^(1+2)
-func isFriedmanNumber(x, y int) {
+func IsFriedmanNumber(x, y int) {
 	return
 }
 
 // $判定関数。与えられた引数が$の場合、その趣旨を表示する
 // $とは「」
 // ex)
-func isXNumber(number int) {
-	return
-}
+// func IsXNumber(number int) {
+// 	return
+// }
+
+
---
+++ cmd/main.go
@@ -0,0 +1,55 @@
+// main
+package main
+
+import (
+	"fmt"
+	"analyze"
+)
+
+
+// 入力を受付、入力された整数の個数に合わせて各解析用関数を呼び出す関数。
+func main() {
+	var inputNumber []int
+	var inputNumberTotal int
+
+	// input
+	fmt.Printf("解析する整数の個数を入力してください>>>")
+	fmt.Scan(&inputNumberTotal)
+	for i := 0; i < inputNumberTotal; i++ {
+		var input int
+		fmt.Printf("%vつ目の整数を入力してください>>>", i+1)
+		fmt.Scan(&input)
+		inputNumber = append(inputNumber, input)
+	}
+
+	fmt.Println("")
+	fmt.Println("Analyze....")
+	switch inputNumberTotal {
+	case 1:
+		analyze.IsOddNumber(inputNumber[0])
+		analyze.IsEvenNumber(inputNumber[0])
+		analyze.IsGrothendieckPrime(inputNumber[0])
+		analyze.IsRepunitNumber(inputNumber[0])
+		analyze.IsPrimeNumber(inputNumber[0])
+		analyze.IsPerfectNumber(inputNumber[0])
+		analyze.IsSquareNumber(inputNumber[0])
+		analyze.IsMersenneNumber(inputNumber[0])
+		analyze.IsTaxicabNumber(inputNumber[0])
+		analyze.IsSophieGermainPrime(inputNumber[0])
+		analyze.IsSafePrime(inputNumber[0])
+		analyze.IsKaprekarNumber(inputNumber[0])
+		analyze.IsFibonacciNumber(inputNumber[0])
+		analyze.IsXNumber(inputNumber[0])
+	case 2:
+		analyze.IsAmicableNumbers(inputNumber[0], inputNumber[1])
+		analyze.IsBetrothedNumber(inputNumber[0], inputNumber[1])
+		analyze.IsSexyPrimes(inputNumber[0], inputNumber[1])
+		analyze.IsTwinPrime(inputNumber[0], inputNumber[1])
+		analyze.IsCousinPrimes(inputNumber[0], inputNumber[1])
+		analyze.GetLeastCommonMultiple(inputNumber[0], inputNumber[1])
+		analyze.GetGreatestCommonDivisor(inputNumber[0], inputNumber[1])
+		analyze.IsFriedmanNumber(inputNumber[0], inputNumber[1])
+	case 3:
+		analyze.IsPythagoreanTriple(inputNumber[0], inputNumber[1], inputNumber[2])
+	}
+}
